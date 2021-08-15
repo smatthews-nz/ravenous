d069ea08c4f067d8eedb04f9cab09d900778282d
Sam Matthews <sam.matthews@gettimely.com>
2021-08-15 17:05:28 +1200

    first commit

---
+++ src/components/BusinessList.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import './BusinessList.css';
+import Business from './Business';
+
+class BusinessList extends Component {
+    render(){
+        return(
+            <div className="BusinessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div>
+        )
+    }
+}
+
+export default BusinessList;
