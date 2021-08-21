8db2950eeec1c57341bdde4791ecd1ae30bf994d
Sam Matthews <sam.matthews@gettimely.com>
2021-08-21 17:26:49 +1200

    Added state to search bar, and mocked search to Yelp API

--- src/app/App.js
+++ src/app/App.js
@@ -26,14 +26,24 @@ const businesses = [
   business,
   business
 ]
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses}/>
-    </div>
-  );
+
+class App extends React.Component {
+
+
+  searchYelp(term, location, sortBy){
+    console.log(`Searching Yelp for ${term} in ${location}, by ${sortBy}`)
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={businesses}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
